30c518add069c3545c1d5c8d680a1ac440ddb3dd
Adam Hoffman <adam@stratospher.es>
2017-11-01 19:50:22 -0500

    Played with various styles of 'import' in the server.ts file - it's hard to find a consistency.

--- build/server.ts
+++ build/server.ts
@@ -1,11 +1,23 @@
 /// <reference path="../node_modules/@types/open/index.d.ts" />
+/// <reference path="../node_modules/@types/node/index.d.ts" />
 
+// only the first of these works... if you try the 2nd, the call to
+// set app = express(); complains that
+// xpress_1.default is not a function
 import express = require('express');
-import path = require('path');
-import open = require('open');
+//import express from "express";
 
-var port = 8080;
-var app = express();
+// either of the next 2 work... why?
+// import path = require('path');
+import * as path from "path";
+
+// on the other hand, only the first one here works - the 2nd one doesn't and throws a
+// "...open/index" resolves to a non-module entity and cannot be imported using this construct.
+import open = require('open');
+// import * as open from "open";
+
+const port = 8080;
+const app = express();
 
 app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
