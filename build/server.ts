41097b3fb61c45b95bb1559777fd12004aabf7ea
Adam Hoffman <adam@stratospher.es>
2017-11-02 23:01:52 -0500

    Lots of fiddling with tslint.json to get to a reasonable set of rules. Rewrites of server.ts and index.ts to conform to those rules.

--- build/server.ts
+++ build/server.ts
@@ -6,8 +6,8 @@
 // only the first of these works... if you try the 2nd, the call to
 // set app = express(); complains that
 // xpress_1.default is not a function
-import express = require('express');
-//import express from "express";
+import express = require("express");
+// import express from "express";
 
 // either of the next 2 work... why?
 // import path = require('path');
@@ -15,29 +15,29 @@ import * as path from "path";
 
 // on the other hand, only the first one here works - the 2nd one doesn't and throws a
 // "...open/index" resolves to a non-module entity and cannot be imported using this construct.
-import open = require('open');
+import open = require("open");
 // import * as open from "open";
 
-import webpack = require('webpack');
+import webpack = require("webpack");
 import config from "../webpack.config.dev";
 
 const port = 8080;
 const app = express();
 const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
+app.use(require("webpack-dev-middleware")(compiler, {
 	noInfo: false,
 	publicPath: config.output.publicPath
 }));
 
-app.get('/', function(req,res) {
-	res.sendFile(path.join(__dirname, '../src/index.html'));
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
-app.listen(port, function(err) {
+app.listen(port, (err) => {
 	if (err) {
 		console.log(err);
 	} else {
-		open('http://localhost:' + port);
+		open("http://localhost:" + port);
 	}
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /// <reference path="../node_modules/@types/numeral/index.d.ts" />
 
-import "./index.css";
 import * as numeral from "numeral";
+import "./index.css";
 
-const testValue = numeral(1000).format('$0,0.00');
+const testValue = numeral(1000).format("0,0.00");
 console.log(`I feel like ${testValue}.`);
